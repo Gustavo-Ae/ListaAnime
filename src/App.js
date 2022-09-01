c0b9f4c40abed6fde72502980a485ce3cca42ba3
GustavoAe <gustavoaedias05@hotmail.com>
2022-09-01 11:02:41 -0300

    listagem simples de animes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import SearchInput from './components/SearchInput/SearchInput';
 
@@ -7,6 +7,24 @@ function App() {
   
   console.log(text)
 
+  const [info, setInfo] = useState({});
+
+  const api = "https://kitsu.io/api/edge"
+
+  useEffect(() => {
+
+    if(text){
+      fetch(`${api}/anime?filter[text]=${text}`)
+        .then((resposta) => resposta.json())
+        .then((resposta) => {
+          setInfo(resposta)
+          // console.log(resposta.data[0].attributes.canonicalTitle)
+          // console.log(resposta)
+        })
+    }
+
+  }, [text])
+
   return (
     <div className="App">
       <h1>Animes</h1>
@@ -14,6 +32,17 @@ function App() {
         value={text} 
         onChange={(search) => setText(search)} 
       />
+
+      {info.data && (
+        <ul>
+            {info.data.map((anime) => (
+                <li key={anime.id}>
+                  {anime.attributes.canonicalTitle}
+                </li>
+            ))}
+        </ul>
+      )}
+
     </div>
   );
 }
