674bbeaf7378deaad1cdcbc4c350f1d12df9e779
GustavoAe <gustavoaedias05@hotmail.com>
2022-09-01 09:38:25 -0300

    verificando se o evento onchange está pegando cada letra do input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import SearchInput from './components/SearchInput/SearchInput';
 
 function App() {
+  const [text, setText] = useState("");
+  
+  console.log(text)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Animes</h1>
+      <SearchInput 
+        value={text} 
+        onChange={(search) => setText(search)} 
+      />
     </div>
   );
 }
